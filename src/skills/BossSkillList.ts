4664de543e7afec40c712812085955339bf355e7
Yunx-x <yun1185448859@qq.com>
2026-02-11 10:45:50 +0800

    弱化Boss技能

--- src/skills/BossSkillList.ts
+++ src/skills/BossSkillList.ts
@@ -3,6 +3,9 @@ import {Skill} from "../base/skill/Skill";
 import {zrand} from "../base/ConstFunc";
 import {getSkillStub} from "../base/skill/HookSkillUtil";
 
+
+export const BOSS_ID = 400055
+
 /**
  * 3207  月神冰
  *
@@ -13,24 +16,32 @@ class Skill3207 extends BaseHookSkillStub {
         super(3207);
     }
 
-     Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
-        skill.GetPlayerNice().SetChihun(120, 6000, 0, 0, 1)
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
+        const player = skill.GetPlayerNice()
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+            skill.GetPlayerNice().SetChihun(120, 6000, 0, 0, 1)
+        }
+
         super.Calculate2(stub, skill, originFunc);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
         const player = skill.GetPlayerNice()
-        const result = zrand(100)
-        const a = result % 3
-        if (a == 0) {
-            player.SetSilent(9999, 6000)
-        } else if (a == 1) {
-            player.SetDizzy(9999, 6000)
+        if (player.GetVar16() == 1) {
+            const result = zrand(100)
+            const a = result % 3
+            if (a == 0) {
+                player.SetSilent(9999, 6000)
+            } else if (a == 1) {
+                player.SetDizzy(9999, 6000)
+            } else {
+                player.SetSuspend(120, 6000, 3, 1, 1)
+            }
+            return true
         } else {
-            player.SetSuspend(120, 6000, 3, 1, 1)
+            return super.StateAttack(stub, skill, originFunc);
         }
-
-        return true
     }
 
 }
@@ -45,21 +56,33 @@ class Skill6296 extends BaseHookSkillStub {
         super(6296);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
         const player = skill.GetPlayerNice()
-        const r = zrand(5)
-        if (r == 1) {
-            player.SetParry2(120, 10000, 0.7, player.GetHp() / 2, 0, 0)
-            player.SetSlant(120, 10000, 1)
-        } else if (r == 2) {
-            player.SetDecay(120, 10000, 0.8, 2)
-            player.SetAvert(120, 10000, 0.001)
-        } else if (r == 3) {
-            player.SetSlant(120, 10000, 0.7)
-            player.SetBuffClearImmune(120, 10000)
-        } else if (r == 4) {
-            player.SetParry2(120, 10000, 0.7, player.GetHp() / 2, 0, 0)
-            player.SetAvert(120, 10000, 0.001)
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+        }
+
+        super.Calculate2(stub, skill, originFunc);
+    }
+
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+        const player = skill.GetPlayerNice()
+        if (player.GetVar16() == 1) {
+            const r = zrand(5)
+            if (r == 1) {
+                player.SetParry2(120, 10000, 0.7, player.GetHp() / 2, 0, 0)
+                player.SetSlant(120, 10000, 1)
+            } else if (r == 2) {
+                player.SetDecay(120, 10000, 0.8, 2)
+                player.SetAvert(120, 10000, 0.001)
+            } else if (r == 3) {
+                player.SetSlant(120, 10000, 0.7)
+                player.SetBuffClearImmune(120, 10000)
+            } else if (r == 4) {
+                player.SetParry2(120, 10000, 0.7, player.GetHp() / 2, 0, 0)
+                player.SetAvert(120, 10000, 0.001)
+            }
+            return true
         }
 
         return super.StateAttack(stub, skill, originFunc);
@@ -77,22 +100,30 @@ class Skill5977 extends BaseHookSkillStub {
         super(5977);
     }
 
-     Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
-        skill.GetPlayerNice().SetIgnoreblessed(120, 3000)
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
+        const player = skill.GetPlayerNice()
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+            player.SetIgnoreblessed(120, 3000)
+        }
+
         super.Calculate2(stub, skill, originFunc);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
         const player = skill.GetPlayerNice()
-        player.SetTurndebuff(15000, 10, 1)
-        player.SetChihun(120, 5000, 0.1, 0, 1)
-        player.SetFrozenImmune(120, 10000)
-        // player.SetAvert(120, 10000, 1)
-        player.SetSlant(120, 8000, 0.8)
-        player.SetBlessed(10, 5000)
+        if (player.GetVar16() == 1) {
+            player.SetTurndebuff(15000, 10, 1)
+            player.SetChihun(120, 5000, 0.1, 0, 1)
+            player.SetFrozenImmune(120, 10000)
+            // player.SetAvert(120, 10000, 1)
+            player.SetSlant(120, 8000, 0.8)
+            player.SetBlessed(10, 5000)
 
-        // return super.StateAttack(stub, skill, originFunc);
-        return true
+            return true
+        }
+
+        return super.StateAttack(stub, skill, originFunc);
     }
 
 }
@@ -120,32 +151,31 @@ class Skill4978 extends BaseHookSkillStub {
         super(4978);
     }
 
-     Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
         const player = skill.GetPlayerNice()
-        // player.SetIgnoreblessed(120, 10000)
-        player.SetChihun(120, 10000, 0.1, 0, 1)
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+            player.SetChihun(120, 10000, 0.1, 0, 1)
+            player.SetAvert(120, 6000, 0.001)
+        }
 
-        // if (player.GetHp() / player.GetMaxhp() <= 0.78) {
-        //     player.SetHeal(120, player.GetHp() * 0.3, 0)
-        // }
-
-        player.SetAvert(120, 6000, 0.001)
-        // player.SetSlant(120, 10000, 0.8)
         super.Calculate2(stub, skill, originFunc);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
         const player = skill.GetPlayerNice()
-        // player.SetDispel(120, 5460, 5461, 5462)
-        // player.SetDispel(120, 116, 116, 116)
-        const result = zrand(2)
-        if (result == 1) {
-            player.SetDirecthurt(120, player.GetMaxhp() * 10)
-        } else if (result == 2) {
-            player.SetMpleak(120, 6000, player.GetMaxmp(), player.GetMp(), 8)
+        if (player.GetVar16() == 1) {
+            const result = zrand(2)
+            if (result == 1) {
+                player.SetDirecthurt(120, player.GetMaxhp() * 10)
+            } else if (result == 2) {
+                player.SetMpleak(120, 6000, player.GetMaxmp(), player.GetMp(), 8)
+            }
+
+            return true
         }
 
-        return true
+        return super.StateAttack(stub, skill, originFunc);
     }
 
 }
@@ -161,19 +191,32 @@ class Skill1775 extends BaseHookSkillStub {
         super(1775);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
         const player = skill.GetPlayerNice()
-        const result = zrand(100)
-        const a = result % 3
-        if (a == 0) {
-            player.SetSuspend(120, 7000, 3, 1, 1)
-        } else if (a == 1) {
-            player.SetFear2(120, 7000, 15)
-        } else {
-            player.SetParalysis(120, 7000, 0.5, 0.5)
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
         }
 
-        return true
+        super.Calculate2(stub, skill, originFunc);
+    }
+
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+        const player = skill.GetPlayerNice()
+        if (player.GetVar16() == 1) {
+            const result = zrand(100)
+            const a = result % 3
+            if (a == 0) {
+                player.SetSuspend(120, 7000, 3, 1, 1)
+            } else if (a == 1) {
+                player.SetFear2(120, 7000, 15)
+            } else {
+                player.SetParalysis(120, 7000, 0.5, 0.5)
+            }
+
+            return true
+        }
+
+        return super.StateAttack(stub, skill, originFunc);
     }
 
 }
@@ -188,18 +231,25 @@ class Skill1089 extends BaseHookSkillStub {
         super(1089);
     }
 
-     Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
-        // const player = skill.GetPlayerNice()
-        // player.SetIgnoreblessed(120, 10000)
-        // player.SetSummon(120, 52420, 8, 8000, 1.0)
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
+        const player = skill.GetPlayerNice()
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+        }
+
         super.Calculate2(stub, skill, originFunc);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
         const player = skill.GetPlayerNice()
-        player.SetRepel(120, skill.GetLevel() + 6)
-        player.SetHpleak7(120, 10000, player.GetMaxhp(), player.GetMaxhp() * 0.8, 0)
-        return true
+        if (player.GetVar16() == 1) {
+            player.SetRepel(120, skill.GetLevel() + 6)
+            player.SetHpleak7(120, 10000, player.GetMaxhp(), player.GetMaxhp() * 0.8, 0)
+
+            return true
+        }
+
+        return super.StateAttack(stub, skill, originFunc);
     }
 
 }
@@ -214,10 +264,23 @@ class Skill1023 extends BaseHookSkillStub {
         super(1023);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
         const player = skill.GetPlayerNice()
-        player.SetAvert(120, skill.GetLevel() * 500 + 100, 0.001)
-        return true
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+        }
+
+        super.Calculate2(stub, skill, originFunc);
+    }
+
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+        const player = skill.GetPlayerNice()
+        if (player.GetVar16() == 1) {
+            player.SetAvert(120, skill.GetLevel() * 500 + 100, 0.001)
+            return true
+        }
+
+        return super.StateAttack(stub, skill, originFunc);
     }
 
 }
@@ -234,21 +297,15 @@ class Skill6549 extends BaseHookSkillStub {
         super(6549);
     }
 
-     Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
         const player = skill.GetPlayerNice()
-        player.SetIgnoreblessed(120, 4000)
-        player.SetIgnoreinvicible(120, 2000)
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetIgnoreblessed(120, 4000)
+            player.SetIgnoreinvicible(120, 2000)
+        }
+
         super.Calculate2(stub, skill, originFunc);
     }
-
-    //  StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
-    //     const player = skill.GetPlayerNice()
-    //     // player.SetHpleak7(120, 10000, player.GetMaxhp() * 2, player.GetMaxhp() * 0.86, 0)
-    //     // player.SetTransform(120, 109912, 1, 1, 30000)
-    //     player.SetDisarrange(9999, 6000, 6000, 20)
-    //     // player.SetAtkchangetarget(120, 8000)
-    //     return super.StateAttack(stub, skill, originFunc);
-    // }
 }
 
 /**
@@ -261,11 +318,22 @@ class Skill4042 extends BaseHookSkillStub {
         super(4042);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
-        super.StateAttack(stub, skill, originFunc);
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
         const player = skill.GetPlayerNice()
-        player.SetDisguise(120, 4000)
-        return true
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+        }
+
+        super.Calculate2(stub, skill, originFunc);
+    }
+
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+        const player = skill.GetPlayerNice()
+        if (player.GetVar16() == 1) {
+            player.SetDisguise(120, 4000)
+        }
+
+        return super.StateAttack(stub, skill, originFunc);
     }
 
 }
@@ -281,19 +349,25 @@ class Skill3917 extends BaseHookSkillStub {
         super(3917);
     }
 
-     Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
         const player = skill.GetPlayerNice()
-        player.SetSkillmirror(120, 6000)
-        player.SetIgnoreblessed(120, 2000)
-        player.SetIgnoreinvicible(120, 2000)
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+            player.SetSkillmirror(120, 6000)
+            player.SetIgnoreblessed(120, 2000)
+            player.SetIgnoreinvicible(120, 2000)
+        }
+
         super.Calculate2(stub, skill, originFunc);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
         const player = skill.GetPlayerNice()
-        // player.SetTransform(120, 109912, 1, 1, 30000)
-        player.SetDisarrange(9999, 4000, 4000, 10)
-        return true
+        if (player.GetVar16() == 1) {
+            player.SetDisarrange(9999, 4000, 4000, 10)
+        }
+
+        return super.StateAttack(stub, skill, originFunc);
     }
 }
 
@@ -306,11 +380,23 @@ class Skill3923 extends BaseHookSkillStub {
         super(3923);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
         const player = skill.GetPlayerNice()
-        player.SetBlessed(10, 3000)
-        player.SetHeal(120, player.GetHp() * 0.05, 0)
-        return true
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+        }
+
+        super.Calculate2(stub, skill, originFunc);
+    }
+
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+        const player = skill.GetPlayerNice()
+        if (player.GetVar16() == 1) {
+            player.SetBlessed(10, 3000)
+            player.SetHeal(120, player.GetHp() * 0.05, 0)
+        }
+
+        return super.StateAttack(stub, skill, originFunc);
     }
 }
 
@@ -324,10 +410,22 @@ class Skill3184 extends BaseHookSkillStub {
         super(3184);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
         const player = skill.GetPlayerNice()
-        player.SetColdinjure(120, 0.2, 10000, 8)
-        return true
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+        }
+
+        super.Calculate2(stub, skill, originFunc);
+    }
+
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+        const player = skill.GetPlayerNice()
+        if (player.GetVar16() == 1) {
+            player.SetColdinjure(120, 0.2, 10000, 8)
+        }
+
+        return super.StateAttack(stub, skill, originFunc);
     }
 
 }
@@ -342,10 +440,23 @@ class Skill3919 extends BaseHookSkillStub {
         super(3919);
     }
 
-     StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+    Calculate2(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>) {
         const player = skill.GetPlayerNice()
-        player.SetSuspend(120, skill.GetLevel() * 1000, 3, 1, 1)
-        return true
+        if (player.GetTmplid() == BOSS_ID) {
+            player.SetVar16(1)
+        }
+
+        super.Calculate2(stub, skill, originFunc);
+    }
+
+    StateAttack(stub: NativePointer, skill: Skill, originFunc: NativeFunction<void, NativePointer[]>): boolean {
+        const player = skill.GetPlayerNice()
+        if (player.GetVar16() == 1) {
+            player.SetSuspend(120, skill.GetLevel() * 1000, 3, 1, 1)
+            return true
+        }
+
+        return super.StateAttack(stub, skill, originFunc);
     }
 
 }
